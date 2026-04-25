29ece436a95c4069dd0249bf5d998a3d46c7810c
Ner2008 <nernani967@gmail.com>
2026-04-25 14:57:27 +0700

    Add files via upload

---
+++ cell01/ex00/name.py
@@ -0,0 +1,3 @@
+first_name = "Preeladol"
+last_name = "Tamtanad"
+print(first_name, last_name)
---
+++ cell01/ex01/name.py
@@ -0,0 +1,5 @@
+first_name = "Preeladol"
+last_name = "Tamtanad"
+whole_name = first_name + " " + last_name
+print(whole_name)
+
