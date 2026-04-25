b2d46aeb6bb762da164ba0c990609401f499471b
Ner2008 <nernani967@gmail.com>
2026-04-25 15:04:06 +0700

    Delete cell02/ex02/i_got_that.py

--- cell02/ex02/i_got_that.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-user_input = input("What you gotta say? : ")
-
-while True:
-    if user_input == "STOP":
-        break
-    
-    user_input = input("I got that! Anything else? : ")
